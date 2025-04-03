54ccfa39c879aab77980a48e0615033a5a52d267
Desaihet123 <154498154+Desaihet123@users.noreply.github.com>
2025-04-03 17:03:04 +0530

    Add files via upload

---
+++ basic-of-js-01/variables.js
@@ -0,0 +1,11 @@
+const accountId = 1234
+let accountEmail = "het209@gmail.com"
+var accountPassword = "1236"
+accountCity = "visnagar"
+
+accountEmail = "ffs@gamil.com"
+
+console.log(accountId);
+console.log(accountEmail);
+
+
