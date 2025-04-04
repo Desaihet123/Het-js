d3776900a7c22719bfd6e199ba2bef8cfcdaae5e
Desaihet123 <154498154+Desaihet123@users.noreply.github.com>
2025-04-04 14:19:48 +0530

    Add files via upload

---
+++ basic-of-js-01/strings.js
@@ -0,0 +1,37 @@
+const name ="het"
+const repocount = 50
+
+console.log(name + repocount + " value");
+
+console.log(`my name is ${name}and my repocount is ${repocount}`)
+
+const gamename = new String("hey fdf")
+
+console.log(gamename[0]);
+console.log(gamename.__proto__);
+
+console.log(gamename.length);
+console.log(gamename.toUpperCase());
+console.log(gamename.charAt(4));
+console.log(gamename.indexOf("y"));
+
+const newstring = gamename.substring(0,4)
+console.log(newstring);
+
+const another = gamename.slice(-5,3)
+console.log(another);
+
+const newstringone = "  het  "
+console.log(newstringone.trim());
+
+const url = "vffjviass/sdndfo/FDFPS"
+console.log(url.replace("sd","het"));
+console.log(url.includes("het"));
+console.log(url.split("/"));
+
+
+
+
+
+
+
