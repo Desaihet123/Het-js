a254467872359fdd5380d474db71ea8bc84fb392
Desaihet123 <154498154+Desaihet123@users.noreply.github.com>
2025-04-04 12:16:39 +0530

    Add files via upload

---
+++ basic-of-js-01/conversion.js
@@ -0,0 +1,46 @@
+let score = "21ab"
+
+console.log(typeof score);
+
+let valueinnum = Number(score)
+
+console.log(typeof valueinnum);
+console.log(valueinnum);
+
+let islogin = 1
+let booleanislogin = Boolean(islogin)
+console.log(booleanislogin);
+
+let st = "conversion"
+let booleanst = Boolean(st)
+console.log(booleanst);
+
+let someNumber = 33
+let stringnum = String(someNumber)
+console.log(typeof stringnum);
+
+//operations
+
+let value = 6
+let negvalue = -value
+console.log(negvalue);
+
+let str1 = "sf"
+let str2 = " esf"
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+ 2 + 2);
+// console.log(2+1+"1");
+
+let gamecounter = 100
+gamecounter++
+console.log(gamecounter);
+
+
+
+
+
+
---
+++ basic-of-js-01/datatypes.js
@@ -0,0 +1,11 @@
+console.log(2+3);
+console.log("het");
+
+let name = "het"
+let age = 19
+let login = true 
+let state;
+
+console.log(typeof state);
+//undefind
+
