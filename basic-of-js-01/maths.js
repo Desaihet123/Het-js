eb3dd0465ac85715eebc7eb6e04685f58a30f7c0
Desaihet123 <154498154+Desaihet123@users.noreply.github.com>
2025-04-04 14:52:39 +0530

    Add files via upload

---
+++ basic-of-js-01/maths.js
@@ -0,0 +1,30 @@
+const score = 400
+console.log(score);
+
+console.log(score.toString().length);
+console.log(score.toFixed(2));
+
+const num = 2653.58
+console.log(num.toPrecision(4));
+
+const hundreds = 10000000
+console.log(hundreds.toLocaleString("en-in"));
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.min(4,9,6,3,5));
+console.log(Math.max(4,8,9,5,3));
+console.log(Math.random());
+
+const min = 20
+const max = 50
+console.log(Math.floor(Math.random() * (max - min + 1)) +min);
+
+
+
+
+
+
+
